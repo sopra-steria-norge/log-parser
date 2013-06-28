dc4546e899468af616918182d7ea3220c42a55d0
Erik Lothe <eriklot@stud.ntnu.no>
2013-06-28 10:59:14 +0200

    Creates module

---
+++ cdms-profile/src/main/java/no/osl/cdms/profile/App.java
@@ -0,0 +1,13 @@
+package no.osl.cdms.profile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
