239d29c04135a6cd9b22e1ddf894ccba714a5962
Nicklas Utgaard <xnsu@steria.no>
2013-07-01 11:09:39 +0200

    Adding @Id to DB duration superclass

--- cdms-profile/src/main/java/no/osl/cdms/profile/log/TimeMeasurementEntity.java
+++ cdms-profile/src/main/java/no/osl/cdms/profile/log/TimeMeasurementEntity.java
@@ -11,6 +11,7 @@ public class TimeMeasurementEntity {
     protected String duration;
 
     @Column(name = "TM_ID")
+    @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private long timeMeasurementId;
 
