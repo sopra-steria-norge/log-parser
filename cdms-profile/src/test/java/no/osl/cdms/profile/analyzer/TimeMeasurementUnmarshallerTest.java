a1a8e0f0c45f56f7a55631cc33f9a5c20f15c082
Nicklas Utgaard <xnsu@steria.no>
2013-07-01 17:48:33 +0200

    Test skeleton, TODO: refactoring function and predicates to a testable class

---
+++ cdms-profile/src/test/java/no/osl/cdms/profile/analyzer/TimeMeasurementUnmarshallerTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.osl.cdms.profile.analyzer;
+
+import java.util.List;
+import java.util.Map;
+import no.osl.cdms.profile.models.TimeMeasurement;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author nutgaard
+ */
+public class TimeMeasurementUnmarshallerTest {
+    
+    public TimeMeasurementUnmarshallerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of unmarshall method, of class TimeMeasurementUnmarshaller.
+     */
+    @Test
+    public void testUnmarshall_List() {
+        System.out.println("unmarshall");
+        List<Map<String, String>> data = null;
+        TimeMeasurementUnmarshaller instance = new TimeMeasurementUnmarshaller();
+        List expResult = null;
+        List result = instance.unmarshall(data);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of unmarshall method, of class TimeMeasurementUnmarshaller.
+     */
+    @Test
+    public void testUnmarshall_List_List() {
+        System.out.println("unmarshall");
+        List<TimeMeasurement> list = null;
+        List<Map<String, String>> data = null;
+        TimeMeasurementUnmarshaller instance = new TimeMeasurementUnmarshaller();
+        List expResult = null;
+        List result = instance.unmarshall(list, data);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
