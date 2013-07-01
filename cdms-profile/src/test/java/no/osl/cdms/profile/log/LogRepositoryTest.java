7984ce4bd194e9fca4623f501cda314f09db0b80
Anders Palfi <anderspalfi@gmail.com>
2013-07-01 16:33:06 +0200

    autowires logrepository to test

--- cdms-profile/src/test/java/no/osl/cdms/profile/log/LogRepositoryTest.java
+++ cdms-profile/src/test/java/no/osl/cdms/profile/log/LogRepositoryTest.java
@@ -2,6 +2,7 @@ package no.osl.cdms.profile.log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,17 +15,21 @@ import javax.persistence.PersistenceContext;
 import static junit.framework.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value = {"classpath:test-cdms-profile-infra-ctx.xml"})
+@ContextConfiguration(value = {"classpath:META-INF/spring/cdms-profile-ctx.xml",
+        "classpath:test-cdms-profile-infra-ctx.xml"})
 @Transactional
 public class LogRepositoryTest {
 
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Autowired
+    private LogRepository logRepository;
+
     @Test
-    public void test() {
+    public void test() throws Exception{
 //        LapEntity lap = entityManager.find(LapEntity.class, 1);
-        assertNotNull(entityManager);
+//        assertNotNull(entityManager);
     }
 
 }
