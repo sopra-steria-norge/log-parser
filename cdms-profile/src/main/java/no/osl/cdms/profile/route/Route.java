3eab99f3c0397b1a3573066939b321f1f54451b9
Nicklas Utgaard <xnsu@steria.no>
2013-07-02 16:47:21 +0200

    Removed some thing that made spring go crazy

--- cdms-profile/src/main/java/no/osl/cdms/profile/route/Route.java
+++ cdms-profile/src/main/java/no/osl/cdms/profile/route/Route.java
@@ -4,35 +4,25 @@
  */
 package no.osl.cdms.profile.route;
 
-import java.util.Map;
-import no.osl.cdms.profile.interfaces.DataAnalyzer;
-import no.osl.cdms.profile.interfaces.Parser;
-import no.osl.cdms.profile.log.TimeMeasurementEntity;
-import no.osl.cdms.profile.parser.DatabaseEntryParser;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 /**
  *
  * @author nutgaard
  */
-@Component
-public class Route implements Processor {            
-    @Autowired
-    private Parser parser;
-    
-    @Autowired
-    private DataAnalyzer analyzer;
-    
-    @Autowired
-    private DatabaseEntryParser db;
-
-    @Override
-    public void process(Exchange exchange) throws Exception {
-        String line = exchange.getIn().getBody(String.class);
-        Map<String, String> parsed = parser.parse(line);
-        TimeMeasurementEntity tme = db.parse(parsed);
-    }
+//@Component
+public class Route {// implements Processor {            
+//    @Autowired
+//    private Parser parser;
+//    
+//    @Autowired
+//    private DataAnalyzer analyzer;
+//    
+//    @Autowired
+//    private DatabaseEntryParser db;
+//
+//    @Override
+//    public void process(Exchange exchange) throws Exception {
+//        String line = exchange.getIn().getBody(String.class);
+//        Map<String, String> parsed = parser.parse(line);
+//        TimeMeasurementEntity tme = db.parse(parsed);
+//    }
 }
