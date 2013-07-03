667dc6f7bd7f58fb9603b7e2d9f5dab00d5b963d
ohelstro <Orjan.Helstrom@gmail.com>
2013-07-03 10:36:49 +0200

    Working on getting webservice for front-end up and running.

--- cdms-profile/src/test/java/no/osl/cdms/profile/webservice/FakeWebServer.java
+++ cdms-profile/src/test/java/no/osl/cdms/profile/webservice/FakeWebServer.java
@@ -2,7 +2,12 @@ package no.osl.cdms.profile.webservice;
 
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.servlet.ServletHolder;
 import org.mortbay.jetty.webapp.WebAppContext;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,12 +22,28 @@ public class FakeWebServer {
 
     public static void main(String[] args) throws Exception{
         final Server server = new Server(PORT);
-        final Context context = new Context(server, "/", Context.REQUEST);
-
-        WebAppContext wac = new WebAppContext("src/main/webapp", "/index.html");
+        final Context context = new Context(server, "/graph", Context.REQUEST);
+        TestServlet ts = new TestServlet();
+        context.addServlet(new ServletHolder(ts), "/*");
+        WebAppContext wac = new WebAppContext("src/main/webapp", "/graph");
         wac.setDescriptor("src/main/webapp/WEB-INF/web.xml");
 
         server.addHandler(wac);
         server.start();
+    }
+
+
+    public static class TestServlet extends HttpServlet{
+
+        public TestServlet()
+        {
+
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        {
+
+        }
     }
 }
