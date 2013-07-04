ec887658d36e3d2a91de36b7b15157212193bed1
Erik Lothe <eriklot@stud.ntnu.no>
2013-07-04 15:21:16 +0200

    Adds JPA route to database

--- cdms-profile/src/main/java/no/osl/cdms/profile/routes/PerformanceLogRoute.java
+++ cdms-profile/src/main/java/no/osl/cdms/profile/routes/PerformanceLogRoute.java
@@ -2,7 +2,6 @@ package no.osl.cdms.profile.routes;
 
 import no.osl.cdms.profile.parser.DatabaseEntryParser;
 import no.osl.cdms.profile.parser.LogLineRegexParser;
-import no.osl.cdms.profile.routes.logentryupdate.LogEntryUpdateProcessor;
 import org.apache.camel.builder.RouteBuilder;
 
 import java.util.Map;
@@ -16,9 +15,9 @@ public class PerformanceLogRoute extends RouteBuilder {
 
     private LogLineRegexParser logLineRegexParser = new LogLineRegexParser();
     private DatabaseEntryParser databaseEntryParser = new DatabaseEntryParser();
-    private LogEntryUpdateProcessor logEntryUpdateProcessor = new LogEntryUpdateProcessor();
 
     private static final String LOG_FILE_ENDPOINT = "stream:file?fileName="+LOG_DIRECTORY +"/"+LOG_FILE+"&scanStream=true&scanStreamDelay=" + DELAY;
+    private static final String DATABASE_ENDPOINT = "jpa:no.osl.cdms.profile.log.TimeMeasurementEntity";
 
     @Override
     public void configure() throws Exception{
@@ -28,8 +27,8 @@ public class PerformanceLogRoute extends RouteBuilder {
                 .convertBodyTo(String.class)                // Converts input to String
                 .choice().when(body().isGreaterThan(""))    // Ignores empty lines
                 .bean(logLineRegexParser, "parse")          // Parses log entry into String map
-                .bean(databaseEntryParser)                  // Parses log entry into database format
-                .bean(logEntryUpdateProcessor)              // Adds log entry to database
+                .bean(databaseEntryParser, "parse")         // Parses log entry into database format
+                .to(DATABASE_ENDPOINT)                      // Adds log entry to database
                 .routeId(PERFORMANCE_LOG_ROUTE_ID);
     }
 
@@ -38,10 +37,15 @@ public class PerformanceLogRoute extends RouteBuilder {
      * Can safely be deleted
      * @param log
      */
-    private void printLogEntry(Map<String, String> log) {
+    public Map<String, String> printLogEntry(Map<String, String> log) {
         for (String key: log.keySet()) {
             System.out.printf("%s\t\t%s\n", key, log.get(key));
         }
         System.out.println();
+        return log;
+    }
+
+    public String toString() {
+        return PERFORMANCE_LOG_ROUTE_ID;
     }
 }
