9f3ba65e59b46c8d182e914391ead35070a93c35
Erik Lothe <eriklot@stud.ntnu.no>
2013-07-04 10:19:50 +0200

    Fixes exceptions of PerformanceLogRouteTest

--- cdms-profile/src/test/java/no/osl/cdms/profile/routes/PerformanceLogRouteTest.java
+++ cdms-profile/src/test/java/no/osl/cdms/profile/routes/PerformanceLogRouteTest.java
@@ -2,40 +2,38 @@ package no.osl.cdms.profile.routes;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value = {"classpath:META-INF/spring/cdms-profile-ctx.xml",
-        "classpath:test-cdms-profile-infra-ctx.xml"})
-public class PerformanceLogRouteTest /*extends CamelSpringTestSupport*/ {
+public class PerformanceLogRouteTest extends CamelSpringTestSupport {
 
-    PerformanceLogRoute performanceLogRoute = new PerformanceLogRoute();
+    PerformanceLogRoute performanceLogRoute;
 
     @Before
     public void setup() throws Exception {
-        /*
-        CamelContext context = new DefaultCamelContext(); // ikke nødvendig ved arv av CamelSpringTestSupport
         performanceLogRoute = new PerformanceLogRoute();
         context.setTracing(true);
         context.addRoutes(performanceLogRoute);
         context.start();
-        */
+
     }
 
     @Test
-    public void routeTest() {
-
+    public void routeTest() throws Exception {
         assert(true);
-
+        context.stop();
     }
 
-    /*@Override
+    @Override
     protected AbstractApplicationContext createApplicationContext() {
         return new ClassPathXmlApplicationContext("classpath:META-INF/spring/cdms-profile-ctx.xml",
                 "classpath:test-cdms-profile-infra-ctx.xml");
-    }*/
+    }
 }
