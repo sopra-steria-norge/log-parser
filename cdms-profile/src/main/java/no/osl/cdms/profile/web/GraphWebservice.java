723152abd123ba49ce6c868716ccc4ad791bca5e
Nicklas Utgaard <xnsu@steria.no>
2013-07-05 09:59:25 +0200

    Local jetty now working

--- cdms-profile/src/main/java/no/osl/cdms/profile/web/GraphWebservice.java
+++ cdms-profile/src/main/java/no/osl/cdms/profile/web/GraphWebservice.java
@@ -1,5 +1,10 @@
 package no.osl.cdms.profile.web;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,8 +27,11 @@ import javax.ws.rs.Produces;
 */
 
 @Path("/")
-public class GraphWebservice {
+public class GraphWebservice extends HttpServlet {
 
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("Page");
+    }
     /**
      *
      * @return
@@ -33,5 +41,10 @@ public class GraphWebservice {
     @Produces("application/json")
     public String getData() {
         return "[name:\"Wait\" , [{x:0.5, y:0.3}, {x:0.7, y: 0.5}]]";
+    }
+
+//    @Override
+    public void handle(String string, HttpServletRequest req, HttpServletResponse resp, int i) throws IOException, ServletException {
+        resp.getWriter().write("This is tha webserver");
     }
 }
