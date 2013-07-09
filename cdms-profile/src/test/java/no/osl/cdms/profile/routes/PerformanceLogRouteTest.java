e2dfe8f1c35ba98e661d48c84bb1cae3a57296b1
Erik Lothe <eriklot@stud.ntnu.no>
2013-07-09 10:03:13 +0200

    Merges

--- cdms-profile/src/test/java/no/osl/cdms/profile/routes/PerformanceLogRouteTest.java
+++ cdms-profile/src/test/java/no/osl/cdms/profile/routes/PerformanceLogRouteTest.java
@@ -1,36 +1,51 @@
 package no.osl.cdms.profile.routes;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.impl.DefaultCamelContext;
+import no.osl.cdms.profile.log.LogRepository;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.annotation.Rollback;
 
+import static org.mockito.Mockito.mock;
 
 public class PerformanceLogRouteTest extends CamelSpringTestSupport {
 
     PerformanceLogRoute performanceLogRoute;
 
-    @Before
-    public void setup() throws Exception {
-//        performanceLogRoute = new PerformanceLogRoute();
-//        context.setTracing(true);
-//        context.addRoutes(performanceLogRoute);
-//        context.start();
+    /*@Autowired
+    LogRepository logRepository;*/
 
+
+    @Before
+    @Rollback(false)
+    public void setup() throws Exception {
+        //performanceLogRoute = new PerformanceLogRoute();
+        //performanceLogRoute.setLogRepository(mock(LogRepository.class));
+        //performanceLogRoute.setLogRepository(logRepository);
+        //context.setTracing(true);
+        //context.addRoutes(performanceLogRoute);
+        //context.start();
     }
 
     @Test
+    @Rollback(false)
     public void routeTest() throws Exception {
-//        assert(true);
-//        context.stop();
+        //Thread.sleep(1000);
+        //assert(true);
+    }
+
+    @After
+    @Rollback(false)
+    public void tearDown() throws Exception {
+        //context.stop();
     }
 
     @Override
     protected AbstractApplicationContext createApplicationContext() {
-        return new ClassPathXmlApplicationContext("classpath:META-INF/spring/cdms-profile-ctx.xml",
+        return new ClassPathXmlApplicationContext("classpath:test-cdms-profile-camel-ctx.xml",
                 "classpath:test-cdms-profile-infra-ctx.xml");
     }
 }
