98648e2064a4d3050fedb815db74f4aaa2c03639
ohelstro <Orjan.Helstrom@gmail.com>
2013-07-18 08:54:47 +0200

    Small refactor.

--- cdms-profile/src/main/webapp/js/application.js
+++ cdms-profile/src/main/webapp/js/application.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     $.get({
+    	//url: 'rest/getLayout/navbar',
         url: 'page/navbar',
         success: function(r) {
             new PageView({model: new PageComponentCollection(JSON.parse(r)), el: 'body'})
         }
     });
     $.get({
+    	//url: 'rest/getLayout/last24h',
         url: 'page/last24h',
         success: function(r) {
             new PageView({model: new PageComponentCollection(JSON.parse(r)), el: '.applicationcontainer'})
