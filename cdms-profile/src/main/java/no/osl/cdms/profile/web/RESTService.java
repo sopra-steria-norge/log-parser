05578dce2de2c84de18bc676a8a6c53c709c23f6
Nicklas Utgaard <xnsu@steria.no>
2013-07-26 13:08:24 +0200

    Updated rest api to support multie id fetching

--- cdms-profile/src/main/java/no/osl/cdms/profile/web/RESTService.java
+++ cdms-profile/src/main/java/no/osl/cdms/profile/web/RESTService.java
@@ -1,5 +1,6 @@
 package no.osl.cdms.profile.web;
 
+import com.google.common.collect.Lists;
 import java.io.StringWriter;
 
 import no.osl.cdms.profile.log.MultiContextEntity;
@@ -15,6 +16,7 @@ import no.osl.cdms.profile.log.TimeMeasurementEntity;
 import org.joda.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import javax.ws.rs.*;
+import no.osl.cdms.profile.interfaces.db.TimeMeasurement;
 
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.JsonGenerator;
@@ -27,18 +29,16 @@ public class RESTService {
 
     @Autowired
     DataRetriever dataRetriever;
-
     Logger logger = Logger.getLogger(getClass().getName());
-
     static int idCounter = 1;
     static int procedureCounter = 1;
     static int multiContextCounter = 1;
     static ProcedureEntity[] procedures = {new ProcedureEntity(null, "Total", null),
-            new ProcedureEntity(null, "IcwMessageProcessorBean", "process"),
-            new ProcedureEntity(null, "FlightServiceImpl", "process"),
-            new ProcedureEntity(null, "Wait", null),
-            new ProcedureEntity(null, "Milestone", "execute"),
-            new ProcedureEntity(null, "UpdateMessageFactory", "createUpdatesForPublishing")};
+        new ProcedureEntity(null, "IcwMessageProcessorBean", "process"),
+        new ProcedureEntity(null, "FlightServiceImpl", "process"),
+        new ProcedureEntity(null, "Wait", null),
+        new ProcedureEntity(null, "Milestone", "execute"),
+        new ProcedureEntity(null, "UpdateMessageFactory", "createUpdatesForPublishing")};
 
     @GET
     @Path("test")
@@ -58,7 +58,7 @@ public class RESTService {
     @Path("percentile")
     @Produces("application/json")
     public String getPercentiles(@QueryParam("from") String from,
-                                 @QueryParam("to") String to, @QueryParam("percentages") String percentages) {
+            @QueryParam("to") String to, @QueryParam("percentages") String percentages) {
         return getPercentiles(null, from, to, percentages);
     }
 
@@ -66,23 +66,25 @@ public class RESTService {
     @Path("percentile/{procedureId}")
     @Produces("application/json")
     public String getPercentiles(@PathParam("procedureId") String procedureId, @QueryParam("from") String from,
-                                 @QueryParam("to") String to, @QueryParam("percentages") String percentages) {
+            @QueryParam("to") String to, @QueryParam("percentages") String percentages) {
         int[] percentagesArray;
         DateTime fromDate;
         DateTime toDate;
-        if (from == null) throw new WebApplicationException(400);
+        if (from == null) {
+            throw new WebApplicationException(400);
+        }
 
         // Parse percentages
         if (percentages == null) {
-            percentagesArray = new int[] {10,20,30,40,50,60,70,80,90};
+            percentagesArray = new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90};
         } else {
             String[] tmp = percentages.split(",");
             percentagesArray = new int[tmp.length];
-            for (int i=0; i<tmp.length; i++) {
+            for (int i = 0; i < tmp.length; i++) {
                 try {
                     percentagesArray[i] = Integer.parseInt(tmp[i].replace(" ", ""));
                 } catch (NumberFormatException e) {
-                    logger.debug("Percentage '"+tmp[i]+"' from user input could not be parsed into int. Percentage ignored.");
+                    logger.debug("Percentage '" + tmp[i] + "' from user input could not be parsed into int. Percentage ignored.");
                 }
             }
         }
@@ -104,8 +106,8 @@ public class RESTService {
         try {
             int procedureIdInt = Integer.parseInt(procedureId);
             return toJSON(dataRetriever.getPercentileByProcedure(procedureIdInt, fromDate, toDate, percentagesArray));
-        } catch (NumberFormatException e){
-            return toJSON(dataRetriever.getPercentile(fromDate,toDate,percentagesArray));
+        } catch (NumberFormatException e) {
+            return toJSON(dataRetriever.getPercentile(fromDate, toDate, percentagesArray));
         }
     }
 
@@ -113,20 +115,25 @@ public class RESTService {
     @Path("timeMeasurement/{procedureId}")
     @Produces("application/json")
     public String getTimeMeasurements(@PathParam("procedureId") String procedureId,
-                                                  @QueryParam("from") String from, @QueryParam("to") String to,
-                                                  @QueryParam("buckets") String buckets) {
-        int procedureIdInt, bucketsInt;
+            @QueryParam("from") String from, @QueryParam("to") String to,
+            @QueryParam("buckets") String buckets) {
+        int[] procedureIdInt;
+        int bucketsInt;
 
         // Parse procedure ID
         try {
-            procedureIdInt = Integer.parseInt(procedureId);
+            String[] splitted = procedureId.split(",");
+            procedureIdInt = new int[splitted.length];
+            for (int i = 0; i < splitted.length; i++) {
+                procedureIdInt[i] = Integer.parseInt(splitted[i]);
+            }
         } catch (NumberFormatException e) {
             logger.debug("procedureId '" + procedureId + "' from user input could not be parsed into int");
-            throw new WebApplicationException(415);
+                throw new WebApplicationException(415);
         }
 
         // Determine number of buckets
-        try{
+        try {
             if (buckets == null) {
                 bucketsInt = -1;
             } else {
@@ -155,38 +162,16 @@ public class RESTService {
 
         // Retrieve and send data
         try {
-            return toJSON(dataRetriever.getTimeMeasurements(procedureIdInt, fromDate, toDate, bucketsInt));
+            List<TimeMeasurement> out = Lists.newLinkedList();
+            for (int id : procedureIdInt){
+                out.addAll(dataRetriever.getTimeMeasurements(id, fromDate, toDate, bucketsInt));
+            }
+            return toJSON(out);
         } catch (NoSuchElementException e) {
             throw new WebApplicationException(404);
         }
     }
 
-
-
-//
-//    @GET
-//    @Path("multicontext")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<MultiContext> getMultiContextsAfterTimestamp(@QueryParam("timestamp") String timestamp) {
-//        List<MultiContext> result = dataRetriever.getMultiContextsAfterTimestamp(timestamp);
-//        return result;
-//    }
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-////        System.out.println("Autowired: "+dataRetriever);
-////        String response = toJSON(dataRetriever.getMultiContextsAfterTimestamp(new DateTime().toString()));
-////        System.out.println("Serialized data: "+response);
-//        JaxRSThis jrt = new JaxRSThis(this);
-//        resp.getWriter().write(jrt.invoke(req, resp));
-////        StringBuilder sb = new StringBuilder();
-////        sb.append("[");
-////        List<TimeMeasurementEntity> tms = new ArrayList<TimeMeasurementEntity>();
-////        for (int i = 0; i < 6; i++) tms.add(createSingle());
-////
-////        resp.getWriter().write(toJSON(tms));
-////        procedureCounter = 1;
-////        multiContextCounter++;
-//    }
     private TimeMeasurementEntity createSingle() {
         MultiContextEntity mc = new MultiContextEntity(new DateTime("2013-06-25T01:15:52.458Z").toDate(),
                 new DateTime("2013-06-25T01:15:52.578Z").toDate());
@@ -194,8 +179,8 @@ public class RESTService {
         pro.setId(procedureCounter);
         mc.setId(multiContextCounter);
         TimeMeasurementEntity tm = new TimeMeasurementEntity(pro, mc, new DateTime().toDate(),
-                new Duration((int)(Math.random()*1000)).toString());
-        if(pro.getClassName().equals("Total")){
+                new Duration((int) (Math.random() * 1000)).toString());
+        if (pro.getClassName().equals("Total")) {
             tm.setDuration(new Duration((int) (Math.random() * 1000 + 2000)).toString());
         }
         idCounter++;
