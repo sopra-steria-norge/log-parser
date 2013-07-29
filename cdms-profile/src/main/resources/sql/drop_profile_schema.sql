97dbe6a5617dba2ffd635c89175051b1b8b63439
Anders Palfi <anderspalfi@gmail.com>
2013-07-29 10:58:18 +0200

    adds indexes from db

--- cdms-profile/src/main/resources/sql/drop_profile_schema.sql
+++ cdms-profile/src/main/resources/sql/drop_profile_schema.sql
@@ -5,3 +5,6 @@ drop table CDM_PROFILE_MULTICONTEXT cascade constraints;
 drop sequence TIMEMEASUREMENT_SEQ;
 drop sequence PROCEDURE_SEQ;
 drop sequence MULTICONTEXT_SEQ;
+
+DROP INDEX timeindex;
+DROP INDEX procedureindex;
